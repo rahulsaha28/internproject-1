c5e52702ce5a5e2029dc77d9d00e46fa1ff77116
Rahul Saha <saharahul039@gmail.com>
2021-06-19 13:59:18 +0600

    fised previous Malic acid problem again.

--- src/Utility/AverageFinder.tsx
+++ src/Utility/AverageFinder.tsx
@@ -1,13 +1,15 @@
-type AverageType = (a:number[][])=>number[]
+type AverageType = (a: number[][]) => number[];
 
 // this is the average finding function from an array
-const AverageFinder:AverageType = (arr)=>{
-    const newArr:number[] = arr.map((newContent:number[])=>newContent.reduce((avg:number, currentElement:number, index:number)=>{
-        avg = (avg+currentElement)/(index+1)
-        return avg;
-     }, 0));
-
-     return newArr;
-}
+const AverageFinder: AverageType = (arr) => {
+  const newArr: number[] = arr.map(
+    (newContent: number[]) =>
+      newContent.reduce(
+        (avg: number, currentElement: number) => avg + currentElement,
+        0
+      ) / newContent.length
+  );
+  return newArr;
+};
 
 export default AverageFinder;
